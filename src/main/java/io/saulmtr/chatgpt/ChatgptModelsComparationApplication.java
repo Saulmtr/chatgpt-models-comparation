ea49209dbbdc6365d6f3dafefeb4e001ce217ab9
Saulmtr <saulmtr@gmail.com>
2023-11-26 01:09:27 +0100

    empty project

---
+++ src/main/java/io/saulmtr/chatgpt/ChatgptModelsComparationApplication.java
@@ -0,0 +1,13 @@
+package io.saulmtr.chatgpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatgptModelsComparationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatgptModelsComparationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/saulmtr/chatgpt/ChatgptModelsComparationApplicationTests.java
@@ -0,0 +1,13 @@
+package io.saulmtr.chatgpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatgptModelsComparationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
